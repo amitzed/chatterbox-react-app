90544ebd1db77b14d1de9aff5fa9f9f778db905a
Amit <amit@AZ-iMac.local>
2018-09-17 16:24:33 -0400

    Added event.target.value in Chat Form and added it to component state

--- src/components/SendChatForm.js
+++ src/components/SendChatForm.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 class SendChatForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      chat: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   handleChange(e) {
-    console.log(e.target.value)
+    this.setState({
+      chat: e.target.value
+    })
   }
 
   render() {
+    console.log(this.state.chat)
     return (
       <form className="send-chat-form">
-        <input onChange={this.handleChange} placeholder="Type and <ENTER> to Send Chat" type="text" />
+        <input onChange={this.handleChange} value={this.state.chat} placeholder="Type and <ENTER> to Send Chat" type="text" />
       </form>
     )
   }
